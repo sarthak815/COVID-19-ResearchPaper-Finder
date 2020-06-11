df1fbf7a1d9d04f5981059935b2b4033ce9e3540
sarthak815 <sarthak815@users.noreply.github.com>
2020-06-11 21:23:43 +0530

    Final

--- app/src/main/java/com/example/paperscrapper/paperview.java
+++ app/src/main/java/com/example/paperscrapper/paperview.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,6 +38,7 @@ public class paperview extends AppCompatActivity {
         ed_journal.setText(journaltext);
         ed_citation.setText(citationtext);
         ed_abstract.setText(abstracttext);
+        ed_abstract.setMovementMethod(new ScrollingMovementMethod());
     }
 
 
